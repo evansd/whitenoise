7f9475ba7dba683debb3d6e7afc827ea1ae6045a
David Evans <d@evans.io>
2016-05-27 16:03:01 +0100

    Send correct headers for 304 responses
    The RFC specifies that the following headers should be sent with a 304
    response, where the equivalent 200 response would include them:
    Cache-Control, Content-Location, Date, ETag, Expires, and Vary.
    
    See: http://tools.ietf.org/html/rfc7232#section-4.1
    
    Fixes #85

--- whitenoise/static_file.py
+++ whitenoise/static_file.py
@@ -15,10 +15,16 @@ Response = namedtuple('Response', ('status', 'headers', 'file'))
 NOT_ALLOWED = Response(HTTPStatus.METHOD_NOT_ALLOWED,
                        (('Allow', 'GET, HEAD'),),
                        None)
-NOT_MODIFIED = Response(HTTPStatus.NOT_MODIFIED, (), None)
 
 ACCEPT_GZIP_RE = re.compile(r'\bgzip\b')
 ACCEPT_BROTLI_RE = re.compile(r'\bbr\b')
+
+# Headers which should be returned with a 304 Not Modified response as
+# specified here: http://tools.ietf.org/html/rfc7232#section-4.1
+NOT_MODIFIED_HEADERS = ('Cache-Control', 'Content-Location', 'Date', 'ETag',
+                        'Expires', 'Vary')
+NOT_MODIFIED_HEADER_RE = re.compile('^({})$'.format(
+        '|'.join(map(re.escape, NOT_MODIFIED_HEADERS))), re.IGNORECASE)
 
 
 class StaticFile(object):
@@ -29,12 +35,13 @@ class StaticFile(object):
         self.gzip_file = file_tuple(gzip_file)
         self.brotli_file = file_tuple(brotli_file)
         self.last_modified = parsedate(headers['Last-Modified'])
+        self.not_modified_response = get_not_modified_response(self.plain_file[1])
 
     def get_response(self, method, request_headers):
         if method != 'GET' and method != 'HEAD':
             return NOT_ALLOWED
         elif self.file_not_modified(request_headers):
-            return NOT_MODIFIED
+            return self.not_modified_response
         path, headers = self.get_path_and_headers(request_headers)
         if method != 'HEAD':
             file_handle = open(path, 'rb')
@@ -86,3 +93,10 @@ def file_tuple(path_and_headers):
     else:
         path, headers = path_and_headers
         return path, tuple(headers.items())
+
+
+def get_not_modified_response(headers):
+    not_modified_headers = tuple([
+        item for item in headers
+        if NOT_MODIFIED_HEADER_RE.match(item[0])])
+    return Response(HTTPStatus.NOT_MODIFIED,  not_modified_headers, None)
