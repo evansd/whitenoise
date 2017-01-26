bc8fe1edb7a350607b2d40104d3ce9b8e2e22b16
David Evans <dave@evans.io>
2017-01-26 22:08:42 +0000

    Temporarily remove invalid PyPI classifier
    This can be reinstated once the below issue is resolved:
    https://github.com/pypa/warehouse/issues/1673

--- setup.py
+++ setup.py
@@ -36,7 +36,9 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
+        # Temporarily removed pending resolution of
+        # https://github.com/pypa/warehouse/issues/1673
+        # 'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
