54f8c86fe7d85f44565498f7c94caabbfdc54a71
David Evans <dave@evans.io>
2017-06-25 14:49:12 +0100

    Re-enable logging to stderr in test settings

--- tests/django_settings.py
+++ tests/django_settings.py
@@ -19,3 +19,27 @@ if django.VERSION >= (1, 10):
     MIDDLEWARE = ['whitenoise.django.WhiteNoiseMiddleware']
 else:
     MIDDLEWARE_CLASSES = ['whitenoise.django.WhiteNoiseMiddleware']
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'log_to_stderr': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['log_to_stderr'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
