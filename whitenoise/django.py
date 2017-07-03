1114f538fb19ac6e2d21849554bd0f1985420c60
David Evans <dave@evans.io>
2017-07-03 16:42:32 +0100

    Raise with upgrade instructions on old import path

---
+++ whitenoise/django.py
@@ -0,0 +1,5 @@
+raise ImportError(
+        '\n\n'
+        'Your WhiteNoise configuration is incompatible with WhiteNoise v4.0\n'
+        'This can be fixed by following the upgrade instructions at:\n'
+        'http://whitenoise.evans.io/en/stable/changelog.html#v4-0\n')
