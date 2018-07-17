2949584cb0734bee3287c451f430e614bc9825e8
Markus Zapke-Gründemann <markus@transcode.de>
2018-07-17 19:15:09 +0200

    Remove Python 3.3 support
    Removed support for Python 3.3 since it's end of life was in
    September 2017.

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
