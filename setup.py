2f273a82bd4df7e3da6ce1e9c610ab6972c2a0e7
David Evans <dave@evans.io>
2020-08-04 10:07:51 +0100

    Add Django 3.1 to supported versions

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
