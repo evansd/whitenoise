042dd970b3f7777954d9c692c5aec1dd6655c7fd
Petr Dlouhý <petr.dlouhy@email.cz>
2021-12-16 18:35:03 +0100

    make possible to set compressor class from settings (or command line argument)

--- src/whitenoise/compress.py
+++ src/whitenoise/compress.py
@@ -6,6 +6,9 @@ import os
 import re
 from io import BytesIO
 
+from django.conf import settings
+from django.utils.module_loading import import_string
+
 try:
     import brotli
 
@@ -134,6 +137,16 @@ class Compressor:
         return filename
 
 
+def get_compressor_class():
+    return import_string(
+        getattr(
+            settings,
+            "WHITENOISE_COMPRESSOR_CLASS",
+            "whitenoise.compress.Compressor",
+        ),
+    )
+
+
 def main(argv=None):
     parser = argparse.ArgumentParser(
         description="Search for all files inside <root> *not* matching "
@@ -151,13 +164,21 @@ def main(argv=None):
         dest="use_gzip",
     )
     parser.add_argument(
+        "--compressor-class",
+        nargs="*",
+        help="Path to compressor class",
+        dest="compressor_class",
+        default="whitenoise.compress.Compressor",
+    )
+    parser.add_argument(
         "--no-brotli",
         help="Don't produce brotli '.br' files",
         action="store_false",
         dest="use_brotli",
     )
     parser.add_argument("root", help="Path root from which to search for files")
-    default_exclude = ", ".join(Compressor.SKIP_COMPRESS_EXTENSIONS)
+    compressor_class = import_string(parser.parse_args(argv).compressor_class)
+    default_exclude = ", ".join(compressor_class.SKIP_COMPRESS_EXTENSIONS)
     parser.add_argument(
         "extensions",
         nargs="*",
@@ -165,11 +186,11 @@ def main(argv=None):
             "File extensions to exclude from compression "
             + f"(default: {default_exclude})"
         ),
-        default=Compressor.SKIP_COMPRESS_EXTENSIONS,
+        default=compressor_class.SKIP_COMPRESS_EXTENSIONS,
     )
     args = parser.parse_args(argv)
 
-    compressor = Compressor(
+    compressor = compressor_class(
         extensions=args.extensions,
         use_gzip=args.use_gzip,
         use_brotli=args.use_brotli,
