ae820af9d66ae19a1e9b666ee5d35a092e16e1eb
Adam Johnson <me@adamj.eu>
2022-01-28 21:02:49 +0000

    Fix URL's for Django 2.0+

--- tests/django_urls.py
+++ tests/django_urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+import django
 from django.http import HttpResponse
 
 
@@ -6,4 +6,11 @@ def hello_world(reqeust):
     return HttpResponse(content="Hello Word", content_type="text/plain")
 
 
-urlpatterns = [url(r"^hello$", hello_world)]
+if django.VERSION >= (2, 0):
+    from django.urls import path
+
+    urlpatterns = [path("hello", hello_world)]
+else:
+    from django.conf.urls import url
+
+    urlpatterns = [url(r"^hello$", hello_world)]
