45e3f808b4d7251d76dbde5bf6b72cf59575fb0d
Adam Johnson <me@adamj.eu>
2022-01-31 09:13:14 +0000

    Set up Django once during test run (#329)

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import os
+
+import django
+import pytest
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup():
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.django_settings"
+    django.setup()
+    yield
