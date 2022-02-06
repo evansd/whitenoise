6a6f083f30b53fd44fd16857b156b5ba83d0a96f
Adam Johnson <me@adamj.eu>
2022-02-06 11:22:03 +0000

    Remove whitenoise.django compatibility module
    This has errored since Whitenoise 4, so it’s reasonable to assume everyone affected has upgraded.

--- whitenoise/django.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-raise ImportError(
-    "\n\n"
-    "Your WhiteNoise configuration is incompatible with WhiteNoise v4.0\n"
-    "This can be fixed by following the upgrade instructions at:\n"
-    "https://whitenoise.evans.io/en/stable/changelog.html#v4-0\n"
-    "\n"
-)
