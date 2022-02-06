6f008ea8e94f136e42d2f231ee2eb456715ca0c1
Adam Johnson <me@adamj.eu>
2022-02-06 11:56:31 +0000

    Increase code coverage (#358)

--- tests/django_urls.py
+++ tests/django_urls.py
@@ -1,11 +1,3 @@
 from __future__ import annotations
 
-from django.http import HttpResponse
-from django.urls import path
-
-
-def hello_world(reqeust):
-    return HttpResponse(content="Hello Word", content_type="text/plain")
-
-
-urlpatterns = [path("hello", hello_world)]
+urlpatterns = []
