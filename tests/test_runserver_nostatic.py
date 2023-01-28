5006e558725d3729f7e260173cc027dec4045573
Adam Johnson <me@adamj.eu>
2023-01-28 12:43:11 +0000

    latest django-stubs

--- tests/test_runserver_nostatic.py
+++ tests/test_runserver_nostatic.py
@@ -7,9 +7,7 @@ from django.core.management import load_command_class
 
 def get_command_instance(name: str) -> BaseCommand:
     app_name = get_commands()[name]
-    # django-stubs incorrect type for get_commands() fixed in:
-    # https://github.com/typeddjango/django-stubs/pull/1074
-    return load_command_class(app_name, name)  # type: ignore [arg-type]
+    return load_command_class(app_name, name)
 
 
 def test_command_output():
